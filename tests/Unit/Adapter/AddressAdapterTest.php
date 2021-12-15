3289f7e3f1c71ed953c933217f98d207e4694587
Arturas <arturas.bespalovas@invertus.eu>
2021-12-15 10:41:21 +0200

    add unit test

---
+++ tests/Unit/Adapter/AddressAdapterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Invertus\dpdBaltics\Adapter\AddressAdapter;
+use PHPUnit\Framework\TestCase;
+
+class AddressAdapterTest extends TestCase
+{
+    /**
+     * @dataProvider getPostCodeData
+     */
+    public function testFormatPostCodeByCountry($postCode, $isoCode, $expectedResult)
+    {
+
+        $addressAdapter = new AddressAdapter();
+        $result = $addressAdapter->formatPostCodeByCountry($postCode, $isoCode);
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function getPostCodeData()
+    {
+        yield 'Post code Lithuania' => [
+            'LT56120',
+            'LT',
+            '56120'
+        ];
+        yield 'Post code Ireland' => [
+            'V42 A393',
+            'IE',
+            'V42A393'
+        ];
+        yield 'Post code Estonia' => [
+            '10EE14A9',
+            'EE',
+            '10149'
+        ];
+        yield 'Post code Latvia' => [
+            'LV-1050',
+            'LV',
+            '1050'
+        ];
+    }
+}
