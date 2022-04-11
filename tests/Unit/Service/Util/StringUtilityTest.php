89a3db1f63d0f8f65698b43d87a5dd0f99717bd8
ernestasluza <ernestas.luza@invertus.eu>
2022-04-11 11:35:48 +0300

    Rewrote unit tests

--- tests/Unit/Service/Util/StringUtilityTest.php
+++ tests/Unit/Service/Util/StringUtilityTest.php
@@ -4,20 +4,20 @@ use Invertus\dpdBaltics\Util\StringUtility;
 
 class StringUtilityTest extends PHPUnit_Framework_TestCase
 {
-            public function testTrimString(){
-                $stringDataMock ="adĖsdĖsdsš";
-                $stringUtility = new StringUtility();
-                $trimmedTestString = $stringUtility->trimString($stringDataMock);
-                $testResult = $trimmedTestString <= $stringUtility::REMARK_LIMIT ? true : false;
-                $this->assertTrue( true, $testResult);
-            }
+    public function testTrimString(){
+        $stringDataMock ="adĖsdĖsdsšsdasdasdasdasdasdasdasdasdasdasdasdasd";
+        $stringUtility = new StringUtility();
+        $trimmedTestString = $stringUtility->trimString($stringDataMock);
+        $expectation = 'adĖsdĖsdsšsdasdasdasdasdasdasdasdasdasdas...';
+        $this->assertSame($trimmedTestString, $expectation);
+    }
 
-           public function testRemoveSpecialCharacters() {
-               $stringDataMock ="adĖsdĖsdsš";
-               $stringUtility = new StringUtility();
-               $formattedTestString = $stringUtility->removeSpecialCharacters($stringDataMock);
-               $isASCIIChar = preg_match( '/[\\x80-\\xff]+/' , $formattedTestString );
-               $this->assertTrue( true, $isASCIIChar);
-           }
+    public function testRemoveSpecialCharacters() {
+        $stringDataMock ="adĖsdĖsdsš";
+        $stringUtility = new StringUtility();
+        $formattedTestString = $stringUtility->removeSpecialCharacters($stringDataMock);
+        $expectation = "adEsdEsdss";
+        $this->assertEquals( $expectation, $formattedTestString);
+    }
 
 }
