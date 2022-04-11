b66266ba44445ff648c57ad075921e628fb6d537
ernestasluza <ernestas.luza@invertus.eu>
2022-04-11 11:56:06 +0300

    readable mockups

--- tests/Unit/Service/Util/StringUtilityTest.php
+++ tests/Unit/Service/Util/StringUtilityTest.php
@@ -5,18 +5,21 @@ use Invertus\dpdBaltics\Util\StringUtility;
 class StringUtilityTest extends PHPUnit_Framework_TestCase
 {
     public function testTrimString(){
-        $stringDataMock ="adĖsdĖsdsšsdasdasdasdasdasdasdasdasdasdasdasdasd";
+        $stringDataMock ="Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus.";
+
+        $expectation = "Nam quis nulla. Integer malesuada. In in eni...";
+
         $stringUtility = new StringUtility();
         $trimmedTestString = $stringUtility->trimString($stringDataMock);
-        $expectation = 'adĖsdĖsdsšsdasdasdasdasdasdasdasdasdasdas...';
+
         $this->assertSame($trimmedTestString, $expectation);
     }
 
     public function testRemoveSpecialCharacters() {
-        $stringDataMock ="adĖsdĖsdsš";
+        $stringDataMock ="vėlyvųjų viduramžių standartas";
         $stringUtility = new StringUtility();
         $formattedTestString = $stringUtility->removeSpecialCharacters($stringDataMock);
-        $expectation = "adEsdEsdss";
+        $expectation = "velyvuju viduramziu standartas";
         $this->assertEquals( $expectation, $formattedTestString);
     }
 
