28a072811db8b54ab125e32bc799b73ab408e321
Gytautas <gytautas.zumaras@invertus.eu>
2024-07-05 14:13:31 +0300

    version bump and changelog added

--- dpdbaltics.php
+++ dpdbaltics.php
@@ -91,7 +91,7 @@ class DPDBaltics extends CarrierModule
         $this->author = 'Invertus';
         $this->tab = 'shipping_logistics';
         $this->description = 'DPD Baltics shipping integration';
-        $this->version = '3.2.19';
+        $this->version = '3.2.20';
         $this->ps_versions_compliancy = ['min' => '1.7.1.0', 'max' => _PS_VERSION_];
         $this->need_instance = 0;
         parent::__construct();
