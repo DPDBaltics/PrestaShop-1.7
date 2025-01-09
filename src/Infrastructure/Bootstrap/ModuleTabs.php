5a9de021b01db10ac111ede9ae59b43ce8038c30
MarijusCoding <marijusdilys9@gmail.com>
2025-01-09 17:22:54 +0200

    [INTERNAL] added ps version if

--- src/Infrastructure/Bootstrap/ModuleTabs.php
+++ src/Infrastructure/Bootstrap/ModuleTabs.php
@@ -1,5 +1,14 @@
 <?php
-
+/**
+ * NOTICE OF LICENSE
+ *
+ * @author    INVERTUS, UAB www.invertus.eu <support@invertus.eu>
+ * @copyright Copyright (c) permanent, INVERTUS, UAB
+ * @license   Addons PrestaShop license limitation
+ * @see       /LICENSE
+ *
+ * International Registered Trademark & Property of INVERTUS, UAB
+ */
 namespace Invertus\dpdBaltics\Infrastructure\Bootstrap;
 
 use Invertus\dpdBaltics\Infrastructure\Adapter\ModuleFactory;
